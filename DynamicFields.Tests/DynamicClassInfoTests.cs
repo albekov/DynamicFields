104d48a5f7f531ad988cf2bbc7738830ccd887a4
oleg <oleg@albekov.ru>
2016-11-20 14:19:08 +0300

    DynamicClassInfoTests

---
+++ DynamicFields.Tests/DynamicClassInfoTests.cs
@@ -0,0 +1,25 @@
+﻿using DynamicFields.Data.Services.Fields;
+using Xunit;
+
+namespace DynamicFields.Tests
+{
+    public class DynamicClassInfoTests
+    {
+        [Fact]
+        public void DynamicClassInfoTest()
+        {
+            Assert.Equal(new DynamicClassInfo(GetType()).Type, GetType());
+        }
+
+        [Fact]
+        public void IsDynamicClassTest()
+        {
+            Assert.False(DynamicClassInfo.IsDynamicClass(typeof(NotDynamicTestClass)));
+            Assert.True(DynamicClassInfo.IsDynamicClass(typeof(DynamicTestClass)));
+        }
+
+        class NotDynamicTestClass { }
+        [DynamicClass]
+        class DynamicTestClass { }
+    }
+}
